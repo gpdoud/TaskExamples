25dfa3cb18f20e22e21fae01f20e95e2ffe296e5
gpdoud <gpdoud@gmail.com>
2022-06-20 07:07:35 -0400

    finished examples

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Task2;
+
+Console.WriteLine("Ready!");
+
+Task2.Utility util = new Task2.Utility { value = 6 };
+
+Task<long> tfactorial = Task<long>.Run(util.Factorial);
+tfactorial.Wait();
+
+Console.WriteLine($"{util.value}! is {tfactorial.Result}");
+
+util.n = 2_000_000_000;
+
+Task<long> tsum = new Task<long>(util.Sum1ToN);
+
+tsum.Start();
+Console.WriteLine("Started ...");
+tsum.Wait();
+Console.WriteLine("Ended ...");
+
+Console.WriteLine($"Sum number 1 to {util.n} is {tsum.Result}");
+
---
+++ Utility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Task2 {
+    public class Utility {
+
+        public int value { get; set; }
+        public long n { get; set; }
+
+        public long Factorial() {
+            long answer = 1;
+            for(int idx = 1; idx <= value; idx++) {
+                answer *= idx;
+            }
+            return answer;
+        }
+
+        public long Sum1ToN() {
+            long sum = 0;
+            for(int i = 1; i <= n; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
+
